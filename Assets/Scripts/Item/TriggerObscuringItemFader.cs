e4e4078ca22051d615dd7956d42a68e2fb4015c0
teradebu <hiroakitakaoki@yahoo.co.jp>
2021-02-20 04:46:25 +0900

    Inventory

---
+++ Assets/Scripts/Item/TriggerObscuringItemFader.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class TriggerObscuringItemFader : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Get the gameObject we have collided with, and then get all the obscuring
+        //Item Fader components on it and its children - and then trigger the fade out
+
+        ObscuringItemFader[] obscuringItemFader =
+            collision.gameObject.GetComponentsInChildren<ObscuringItemFader>();
+
+        if (obscuringItemFader.Length > 0)
+        {
+            for(int i = 0; i < obscuringItemFader.Length; i++)
+            {
+                obscuringItemFader[i].FadeOut();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ObscuringItemFader[] obscuringItemFader = 
+            collision.gameObject.GetComponentsInChildren<ObscuringItemFader>();
+
+        if (obscuringItemFader.Length > 0)
+        {
+            for(int i = 0; i < obscuringItemFader.Length; i++)
+            {
+                obscuringItemFader[i].FadeIn();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+
+    /// <summary>
+    /// 各シーンにおけるカメラ用の境界線を取得し、シネマシーンにセットする。
+    /// </summary>
+    private void SwitchBoundingShape()
+    {
+        PolygonCollider2D polygonCollider2D = 
+            GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).
+            GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        cinemachineConfiner.InvalidatePathCache();
+    }
+
+
+}
